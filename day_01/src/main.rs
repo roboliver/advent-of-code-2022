bfec7da44fbf1318055d3dc931eec8982b2ce0c5
roboliver <oliver.rw93@gmail.com>
2022-12-05 00:56:54 +0000

    renamed functions for clarity

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -3,18 +3,18 @@ use core::str;
 
 fn main() {
     let input = read_input();
-    println!("Part 1: {}", max_calories(&input));
-    println!("Part 2: {}", max_three_calories(&input));
+    println!("Part 1: {}", max_elf_calories(&input));
+    println!("Part 2: {}", max_three_elf_calories(&input));
 }
 
-fn max_calories(input: &str) -> u32 {
+fn max_elf_calories(input: &str) -> u32 {
     calories_per_elf(&input)
         .into_iter()
         .max()
         .expect("No elves found")
 }
 
-fn max_three_calories(input: &str) -> u32 {
+fn max_three_elf_calories(input: &str) -> u32 {
     let mut calories = calories_per_elf(&input);
     calories.sort();
     calories.reverse();
